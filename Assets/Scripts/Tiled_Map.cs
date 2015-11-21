27ec693283424e7f2135639fe0ec54dcfcb246e7
minh-quang <nguyenminhquang13@gmail.com>
2015-11-21 02:19:20 +0100

    Tiled Map created - script TODO

---
+++ Assets/Scripts/Tiled_Map.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiled_Map : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
