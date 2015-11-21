53f88596abdb6e990aaf2529e4b7a1f32ba1de9a
Mathilde GILBERT <mathilde.gilbert@ensiie.fr>
2015-11-21 23:17:53 +0100

    Ajout du menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+  public void ChangeScene(string Scene)
+    {
+        Application.LoadLevel(Scene);
+    }
+
+    public void QuitJeux()
+    {
+        Application.Quit();
+    }
+}
