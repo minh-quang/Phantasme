12fad09c3cb48c7378cbbf75722b1dc0943ee99c
minh-quang <nguyenminhquang13@gmail.com>
2015-11-21 20:33:39 +0100

    commit before merge

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -25,7 +25,33 @@ public class MapManager : MonoBehaviour {
             {
                 int rand = Random.Range(0, prefab.Length - 1);
 
-                Tiled_Map clone = Instantiate(prefab[rand], new Vector3(x * 10.0f, y * 10.0f, 0), Quaternion.identity) as Tiled_Map;
+                Quaternion rotation = Quaternion.identity;
+
+                if (x == 0)
+                {
+                    if (y == 0)
+                    {
+                        rotation = Quaternion.identity;
+                    }
+                    else if (y == 1)
+                    {
+                        rotation = Quaternion.FromToRotation(new Vector3(1,0,0), new Vector3(0,1,0));
+                    }
+                }
+                else if (x == 1)
+                {
+                    if (y == 0)
+                    {
+                        rotation = Quaternion.FromToRotation(new Vector3(1, 0, 0), new Vector3(0, -1, 0));
+                    }
+                    else if (y == 1)
+                    {
+                        //rotation = Quaternion.FromToRotation(Vector3.right, Vector3.left);
+                        rotation = Quaternion.Euler(0, 0, 180f);
+                    }
+                }
+ 
+                Tiled_Map clone = Instantiate(prefab[rand], new Vector3(x * 4.8f, -y * 4.8f, 0), rotation) as Tiled_Map;
                 //clone.GetComponent<SpriteRenderer>().sprite = clone.GetComponent<Tiled_Map>().getSprite();
             }
         }
